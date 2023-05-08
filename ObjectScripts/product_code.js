99fcba88873d416ba85f72db070555e850d5d26e
davempark09 <43024345+davempark09@users.noreply.github.com>
2023-05-08 15:39:06 -0400

    Minor fixes

--- ObjectScripts/product_code.js
+++ ObjectScripts/product_code.js
@@ -10,5 +10,9 @@ function parsePartCode(product) {
     }
     return newPart;
 }
-console.log(parsePartCode('supplierCode:productNumber-size'))
-console.log(parsePartCode('XYZ:1234-L'))
+
+let partCode1 = "XYZ:1234-L";
+let part1 = parsePartCode(partCode1);
+console.log(part1);
+console.log("Supplier: " + part1.supplierCode +" Product Number: " 
++ part1.productNumber +" Size: " + part1.size);
